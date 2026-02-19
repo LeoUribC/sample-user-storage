1b46d29f1e36918aa7ebd89d81b525504fb87116
LeoUribC <leo.uribe1205@gmail.com>
2026-02-19 11:03:33 -0500

    testing invalid email on user creation

--- tests/test_services.py
+++ tests/test_services.py
@@ -27,6 +27,7 @@ def test_create_user_duplicate_id():
     """Verifica que se arroje un error si se crea un user con un id
     existente
     """
+
     mock_storage = MagicMock()
     mock_storage.load.return_value = [
         User(id=1, name="Existing", email="existing@test.com")
@@ -63,3 +64,16 @@ def test_get_user_not_found():
 
     with pytest.raises(UserNotFoundError):
         service.get_user(99)
+
+
+def test_create_user_invalid_email():
+    """Verifica que se lance un error con un email invalido"""
+    mock_storage = MagicMock()
+    service = UserService(mock_storage)
+
+    user = User(id=1, name="Leo", email="invalid-email")
+
+    with pytest.raises(InvalidUserDataError) as exec_info:
+        service.create_user(user)
+
+    assert "Invalid email address" in str(exec_info.value)
